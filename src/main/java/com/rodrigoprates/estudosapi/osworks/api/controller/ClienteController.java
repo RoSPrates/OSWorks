b18916cec04e5ed460f738a2abc17c0c418208e5
Rodrigo Prates <rodrigo.prates@yaman.com.br>
2020-06-21 16:25:09 -0300

    iniciando o repositorio

---
+++ src/main/java/com/rodrigoprates/estudosapi/osworks/api/controller/ClienteController.java
@@ -0,0 +1,22 @@
+package com.rodrigoprates.estudosapi.osworks.api.controller;
+
+import com.rodrigoprates.estudosapi.osworks.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes")
+    public List<Cliente> listar() {
+        Cliente c1 = new Cliente(1l,"João","jaozinhodasneves@estudandoparadevjava.com","(11) 91111-1111");
+        Cliente c2 = new Cliente(2l,"Ash","ash10anosparasempre@estudandoparadevjava.com","(21) 92222-2222");
+        Cliente c3 = new Cliente(3l,"Ricko","rickoemorto@estudandoparadevjava.com","(31) 93333-3333");
+        Cliente c4 = new Cliente(4l,"Mario","quemario@estudandoparadevjava.com","(11) 94444-4444");
+
+        return Arrays.asList(c1, c2, c3, c4);
+    }
+}
---
+++ src/test/java/com/rodrigoprates/estudosapi/osworks/api/OsworksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoprates.estudosapi.osworks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsworksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
